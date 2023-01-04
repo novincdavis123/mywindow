1f35f1edd66dda0bb36d97dca091afa7d5ef5808
albinjoy009 <albinjoy009@gmail.com>
2023-01-04 14:20:00 +0530

    commit

---
+++ bin/fibo.dart
@@ -0,0 +1,12 @@
+void main(){
+  var n1=0,
+  n2=1;
+  print(n1);
+  print(n2);
+  for(int i=2;i<=10;i++){
+    int n3=n1+n2;
+    print(n3);
+    n1=n2;
+    n2=n3;
+  }
+}
--- bin/whiledowhile.dart
+++ bin/whiledowhile.dart
@@ -1,7 +1,12 @@
 void main(){
   int num=10;
-  while(num>=1){
+  while(num==1){
     print(num);
     num--;
   }
+  int i=10;
+  do{
+    print(i);
+    i--;
+  }while(i==1);
 }
