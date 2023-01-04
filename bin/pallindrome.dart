0921cb1c97411e81a120c528a8506e9ab12f06e0
albinjoy009 <albinjoy009@gmail.com>
2023-01-04 15:33:34 +0530

    commit

--- bin/pallindrome.dart
+++ bin/pallindrome.dart
@@ -1,8 +1,15 @@
 void main(){
-  int num=423;
+  int num=121;
   int rev=0;
   int n=num;
   while(num>0){
     int rem=num%10;
+    rev=(rev*10)+rem;
+    num=num~/10;
+  }
+  if(n==rev){
+    print('pallindrome number');
+  }else{
+    print('Not pallindrome number');
   }
 }
