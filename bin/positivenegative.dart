58ca1eec0761cad4b0d653252f5061ec798768a6
albinjoy009 <albinjoy009@gmail.com>
2023-01-05 14:56:44 +0530

    commit

---
+++ bin/positivenegative.dart
@@ -0,0 +1,18 @@
+void main(){
+  var list = [1, -3, 7, 9, 0, -6, 4, -2, 0, 10, -8, 5];
+ int pcount=0;
+ int ncount=0;
+ int zcount=0;
+  for (int i= 0; i < list.length; i++) {
+    if(list[i]>0) {
+      pcount++;
+    }else if(list[i]<0){
+      ncount++;
+  }else{
+      zcount++;
+    }
+}
+  print('positive number is $pcount');
+  print('negative number is $pcount');
+  print('zero number is $pcount');
+}
